8d63c78e269235ba02f332cac1139223fdabfa8a
Joshua <jschmidtnj@gmail.com>
2021-01-11 14:03:14 -0500

    cat: added meow

--- animal.py
+++ animal.py
@@ -1,8 +1,18 @@
+from sys import argv
+
+def cat():
+    print('meow')
+
 def default():
     print('hello')
 
 def main():
-    default()
+    if len(argv) == 1:
+        default()
+    elif argv[1] == 'cat':
+        cat()
+    else:
+        default()
 
 if __name__ == '__main__':
     main()
