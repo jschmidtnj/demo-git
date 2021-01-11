0ab19d6b4cd0f2150e13fe31beaaa7ea82f40a8b
Joshua <jschmidtnj@gmail.com>
2021-01-11 14:06:30 -0500

    animal: added dog logic

--- animal.py
+++ animal.py
@@ -1,16 +1,30 @@
 from sys import argv
 
+<<<<<<< HEAD
 def cat():
     print('meow')
 
+=======
+>>>>>>> 767e0f4 (animal: added dog logic)
 def default():
     print('hello')
 
+def dog():
+    print('bark')
+    print('woof')
+
 def main():
+<<<<<<< HEAD
     if len(argv) == 1:
         default()
     elif argv[1] == 'cat':
         cat()
+=======
+    if len(argv) < 2:
+        default()
+    elif argv[1] == 'dog':
+        dog()
+>>>>>>> 767e0f4 (animal: added dog logic)
     else:
         default()
 
